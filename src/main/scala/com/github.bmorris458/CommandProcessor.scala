a97fce0cbdcde43891518f3b9f1d8af77fd48fe0
Benjamin Morris <bmorris458@gmail.com>
2016-06-07 01:05:26 -0600

    Info on recovery failure.

--- src/main/scala/com/github.bmorris458/CommandProcessor.scala
+++ src/main/scala/com/github.bmorris458/CommandProcessor.scala
@@ -45,6 +45,16 @@ class CommandProcessor extends PersistentActor {
   }
 
   // Not very dry. Fix once everything is running as expected.
+  /* Currently failing. Most recent trace:
+        [info] [ERROR] [06/07/2016 00:58:57.866] [on-spray-can-akka.actor.default-dispatcher-3] [akka://on-spray-can/user/Sarge]
+        Exception in receiveRecover when replaying event type [com.github.bmorris458.market.processors.common.UserAdded] with sequence number [1] for persistenceId [Sarge].
+        [info] java.lang.NullPointerException
+        [info]  at com.github.bmorris458.market.processors.CommandProcessor.sendEvent(CommandProcessor.scala:59)
+        [info]  at com.github.bmorris458.market.processors.CommandProcessor$$anonfun$receiveRecover$1.applyOrElse(CommandProcessor.scala:49)
+  */
+  //Aha! not sure what to do about it, but it's because it's trying to replay
+  //before the queryProcessor has been identified, so the message target is null.
+  //@todo: figure out a way to hook that up before the recovery begins.
   override def receiveRecover: Receive = {
     case evt: UserAdded => sendEvent(evt)
     case evt: UserRemoved => sendEvent(evt)
